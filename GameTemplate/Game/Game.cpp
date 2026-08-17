08683938965763a2f8390ee9c45e6ba545e36799
YamaguchiHayato <CA01244029@st.kawahara.ac.jp>
2026-08-17 09:24:15 +0900

    <fix> 開発エンジンの初期化が不十分であったため修正

--- GameTemplate/Game/Game.cpp
+++ GameTemplate/Game/Game.cpp
@@ -1,38 +1,18 @@
 #include "stdafx.h"
 #include "Game.h"
 #include "GameCamera.h"
-#include "RhythmGame.h"
-#include "SelectMusical.h"
-#include "SceneManager.h"
-
 
 Game::Game() = default;
 
 Game::~Game()
 {
-	DeleteGO(m_skyCube);
-	DeleteGO(m_camera);
-	DeleteGO(m_sceneManager);
 }
 
 bool Game::Start()
 {
-
-	NewGOStartGameObject();
-	
 	return true;
 }
 void Game::Update() {
 
 }
 
-void Game::NewGOStartGameObject()
-{
-	m_skyCube = NewGO<SkyCube>(0, "skyCube");
-	m_skyCube->SetLuminance(1.0f);
-	m_skyCube->SetScale(700.0f);//4000倍にすると描画がなくなるかも。
-	m_camera = NewGO<GameCamera>(0, "camera");
-	m_camera->Init();
-	m_sceneManager = NewGO<SceneManager>(0, "SceneManager");
-	m_sceneManager->Init();
-}
